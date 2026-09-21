a0a0821586cc0aed879aa48368eb17ebad93dad2
Anoop <anoopkumar636363@gmail.com>
2026-09-21 23:23:30 +0530

    Centralize supported target languages

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+ALLOWED_LANGUAGES = {
+    "Kannada": "kn",
+    "Hindi": "hi",
+    "Telugu": "te",
+    "Tamil": "ta",
+    "Marathi": "mr",
+    "Malayalam": "ml",
+    "Bengali": "bn",
+    "Gujarati": "gu",
+    "Punjabi": "pa",
+    "English": "en",
+}
